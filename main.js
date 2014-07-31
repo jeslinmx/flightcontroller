8c710607f57a9c6903034032df93c3948eaa4233
Jeshua Lin <em0sl4y3r@gmail.com>
2014-07-31 10:23:20 +0000

    argv

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var outputHandler = require('./unityOut.js');
 var input = require('./netIn.js');
 var controller = require('./admin.js');
-outputHandler.init(8080, 8081, controller);
+var argv = require("minimist")(process.argv.slice(2));
+var outputPort = argv.o || 3000, sockpolPort = argv.s || 4000, inputPort = argv.i || 8080;
+outputHandler.init(outputPort, sockpolPort, controller);
 controller.init(input);
-input.init(8888, outputHandler, null, controller);
+input.init(inputPort, outputHandler, null, controller);
